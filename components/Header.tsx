2535bb6ba146c93b0b2696b873b21153dbde6c00
fabio4520 <fabioleofc@gmail.com>
2022-10-06 10:19:15 -0500

    ADD animation for header

--- components/Header.tsx
+++ components/Header.tsx
@@ -1,12 +1,27 @@
 import React from 'react'
 import { SocialIcon } from 'react-social-icons'
+import { motion } from "framer-motion"
 
 type Props = {}
 
 export default function Header({}: Props) {
   return (
     <header className='sticky top-0 p-5 flex items-start justify-between max-w-7xl mx-auto z-30 xl:items-center'>
-      <div>
+      <motion.div
+        initial={{
+          x: -500,
+          opacity: 0,
+          scale: 0.5
+        }}
+        animate={{
+          x: 0,
+          opacity: 1,
+          scale: 1
+        }}
+        transition = {{
+          duration: 1.5
+        }}
+        className='flex flex-row items-center'>
         {/* Social icons */}
         <SocialIcon
           url='https://www.linkedin.com/in/fabio-fiestas/'
@@ -21,10 +36,24 @@ export default function Header({}: Props) {
           fgColor='gray'
           bgColor='transparent'
         />
-      </div>
+      </motion.div>
 
       {/* Avatar */}
-      <div className='flex flex-row items-center text-gray-300 cursor-pointer'>
+      <motion.div
+        initial={{
+          x: 500,
+          opacity: 0,
+          scale: 0.5
+        }}
+        animate={{
+          x: 0,
+          opacity: 1,
+          scale: 1
+        }}
+        transition={{
+          duration: 1.5
+        }}
+        className='flex flex-row items-center text-gray-300 cursor-pointer'>
         <SocialIcon
           className='cursor-pointer'
           network='email'
@@ -32,7 +61,7 @@ export default function Header({}: Props) {
           bgColor='transparent'
         />
         <p className='uppercase hidden md:inline-flex text-sm text-gray-400'>Let&apos;s work together!</p>
-      </div>
+      </motion.div>
     </header>
   )
 }
