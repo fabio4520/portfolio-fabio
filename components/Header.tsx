d84bc4e7f742843c7a2444a15ed5afa241f34f97
fabio4520 <fabioleofc@gmail.com>
2022-10-06 10:06:40 -0500

    ADD initial styles for header icons

--- components/Header.tsx
+++ components/Header.tsx
@@ -5,8 +5,34 @@ type Props = {}
 
 export default function Header({}: Props) {
   return (
-    <header>
-      
+    <header className='sticky top-0 p-5 flex items-start justify-between max-w-7xl mx-auto z-30 xl:items-center'>
+      <div>
+        {/* Social icons */}
+        <SocialIcon
+          url='https://www.linkedin.com/in/fabio-fiestas/'
+          fgColor='gray'
+          bgColor='transparent'
+        />
+        <SocialIcon url='https://www.instagram.com/fabio4520/'
+          fgColor='gray'
+          bgColor='transparent'
+        />
+        <SocialIcon url='https://github.com/fabio4520'
+          fgColor='gray'
+          bgColor='transparent'
+        />
+      </div>
+
+      {/* Avatar */}
+      <div className='flex flex-row items-center text-gray-300 cursor-pointer'>
+        <SocialIcon
+          className='cursor-pointer'
+          network='email'
+          fgColor='gray'
+          bgColor='transparent'
+        />
+        <p className='uppercase hidden md:inline-flex text-sm text-gray-400'>Let&apos;s work together!</p>
+      </div>
     </header>
   )
 }
--- pages/index.tsx
+++ pages/index.tsx
@@ -6,7 +6,7 @@ const Home: NextPage = () => {
   return (
     <div>
       <Head>
-        <title>Fabios portfolio</title>
+        <title>Fabio&apos;s portfolio</title>
       </Head>
       
       {/* Header */}
