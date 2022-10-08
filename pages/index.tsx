3f8584d9d99e70c2595fe84094998b009900913d
fabio4520 <fabioleofc@gmail.com>
2022-10-08 17:46:50 -0500

    ADD: home button

--- pages/index.tsx
+++ pages/index.tsx
@@ -7,6 +7,8 @@ import Hero from '../components/Hero'
 import Projects from '../components/Projects'
 import Skills from '../components/Skills'
 import WorkExperience from '../components/WorkExperience'
+import { ArrowUpCircleIcon } from '@heroicons/react/24/solid'
+import Link from 'next/link'
 
 const Home: NextPage = () => {
   return (
@@ -47,6 +49,16 @@ const Home: NextPage = () => {
       <section id='contact' className='snap-start'>
         <ContactMe/>
       </section>
+
+      <Link href="#hero">
+        <footer className='sticky bottom-5 w-full cursor-pointer'>
+          <div className='flex items-center justify-center'>
+          <ArrowUpCircleIcon
+           className='h-10 w-10 rounded-full filter text-[#f7ab0a]/40 hover:text-[#f7ab0a]'
+          />
+          </div>
+        </footer>
+      </Link>
 
     </div>
   )
