e28c5df544630d7d9982b4a468ff5a657c5cf6a6
fabio4520 <fabioleofc@gmail.com>
2022-10-07 20:16:56 -0500

    CHORE: typo in skills

--- components/Skills.tsx
+++ components/Skills.tsx
@@ -14,7 +14,7 @@ export default function Skills({}: Props) {
         className='flex relative flex-col text-center md:text-left xl:flex-row max-w-[2000px] xl:px-10 min-h-screen justify-center xl:space-y-0 mx-auto items-center'>
         <h3 className='absolute top-24 uppercase tracking-[20px] text-gray-500 text-2xl'>Skills</h3>
 
-        <h3 className='absolute top-36 uppercase tracking-[3px] text-gray-500 text-sm'>Hover a skill for currency proficiency</h3>
+        <h3 className='absolute top-36 uppercase tracking-[3px] text-gray-500 text-sm'>Hover a skill for currently proficiency</h3>
 
         <div className='grid grid-cols-4 gap-5 pt-14'>
           <Skill proficiency={84} directionLeft imageLink='https://camo.githubusercontent.com/fb369e3a940701ea56e03aa4dacd7385d141734c3d97258234c4fb84256468d9/68747470733a2f2f63646e2e6a7364656c6976722e6e65742f67682f64657669636f6e732f64657669636f6e2f69636f6e732f727562792f727562792d6f726967696e616c2e737667'/>
