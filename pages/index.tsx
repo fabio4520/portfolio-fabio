e5e46023c05165495195a1fa2b1a7495904440a9
fabio4520 <fabioleofc@gmail.com>
2022-10-07 20:13:54 -0500

    ADD: custom scrollbar

--- pages/index.tsx
+++ pages/index.tsx
@@ -9,7 +9,7 @@ import Skills from '../components/Skills'
 
 const Home: NextPage = () => {
   return (
-    <div className='bg-[rgb(18,22,25)] h-screen snap-y snap-mandatory overflow-y-scroll overflow-x-hidden z-0'>
+    <div className='bg-[rgb(18,22,25)] h-screen snap-y snap-mandatory overflow-y-scroll overflow-x-hidden z-0 scrollbar scrollbar-track-gray-400/20 scrollbar-thumb-[#f7ab0a]/80'>
       <Head>
         <title>Fabio&apos;s portfolio</title>
       </Head>
