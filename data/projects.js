3bfabb37974d1aa2e0da44cdb073660648d6e731
fabio4520 <fabioleofc@gmail.com>
2022-10-09 01:38:44 -0500

    chore: change images links for projects section

--- data/projects.js
+++ data/projects.js
@@ -1,15 +1,15 @@
 const projects = [
   {
     "name": "Contactable",
-    "img": "https://i.im.ge/2022/10/09/1G7KrG.tweetable-individual.png",
-    "info": "This is a web application that attempt to be a replicate of Twitter. A person can sign up with an email. If the user is not logged, he can see all tweets and retweets but he can’t create one. Once the user is logged, can make all the tweets he wants. I hope you enjoy the app!",
+    "img": "https://i.im.ge/2022/10/09/1G5XZ6.home-contactable-new.png",
+    "info": "Keep track of contact emails and phone numbers in your local memory (your brain) is something from the past. Contactable make your life easier keeping track of all that information for you. Feel free of create new user and add new contacts. I hope you enjoy it 😉",
     "link_code": "",
     "link_web": "https://thunderous-jalebi-a269c6.netlify.app"
   },
   {
     "name": "Keepable",
-    "img": "https://mellifluous-madeleine-455192.netlify.app/assets/img/tweetable%20individual.png",
-    "info": "This is a web application that attempt to be a replicate of Twitter. A person can sign up with an email. If the user is not logged, he can see all tweets and retweets but he can’t create one. Once the user is logged, can make all the tweets he wants. I hope you enjoy the app!",
+    "img": "https://i.im.ge/2022/10/09/1G5rXF.home-keepable.png",
+    "info": "Are you looking for a place where you can create notes, edit them, delete them and even restorage them? Keepable is a take-nothing service and productivity app designed by Codeable students in which users can post amazing notes. I hope you enjoy it 😉",
     "link_code": "",
     "link_web": "https://soft-eclair-54ce75.netlify.app"
   },
@@ -22,8 +22,8 @@ const projects = [
   },
   {
     "name": "Organizable",
-    "img": "https://mellifluous-madeleine-455192.netlify.app/assets/img/tweetable%20individual.png",
-    "info": "This is a web application that attempt to be a replicate of Twitter. A person can sign up with an email. If the user is not logged, he can see all tweets and retweets but he can’t create one. Once the user is logged, can make all the tweets he wants. I hope you enjoy the app!",
+    "img": "https://i.im.ge/2022/10/09/1G5AGG.home-organizable.png",
+    "info": "Keep track of all your projects and task with this revolutionary, never seen before, application: organizable.",
     "link_code": "",
     "link_web": "https://chipper-fudge-018a2b.netlify.app"
   },
