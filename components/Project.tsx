5342b9135d2a7e10ca91f877006de59c18525a61
fabio4520 <fabioleofc@gmail.com>
2022-12-26 11:53:23 -0500

    ADD: fetch info in projects section

--- components/Project.tsx
+++ components/Project.tsx
@@ -1,38 +1,34 @@
 import React from 'react'
 import Link from 'next/link';
 import { motion } from 'framer-motion'
+import { Project as ProjectType } from '../typings';
+import { urlFor } from '../sanity';
 
-type Props = {}
-
-type ProjectsType = {
-  name: string,
-  img: string,
-  info: string,
-  link_code: string,
-  link_web: string
+type Props = {
+  project: ProjectType;
 }
 
 
-export default function Project( project: ProjectsType ) {
+export default function Project({ project }: Props ) {
   return (
     <div>
       <motion.img
         initial={{ y: -300, opacity: 0 }}
         transition={{duration: 1.5}}
         whileInView={{ opacity: 1, y: 0 }}
-        src={ project.img }
+        src={ urlFor(project?.image).url() }
         className='max-w-lg md:max-w-md sm:max-w-sm' alt="" />
       <div className='space-x-6 mt-5 text-center'>
-        <Link href={ project.link_code }>
+        <Link href={ project.linkToSourceCode ? project.linkToSourceCode : '#' }>
           <button
-            type='submit'
+            type='button'
             className='bg-[#f7ab0a] py-2 px-5 rounded-md text-black font-bold text-lg'>
               View source code
             </button>
         </Link>
-        <Link href={ project.link_web }>
+        <Link href={ project?.linkToWebsite }>
           <button
-            type='submit'
+            type='button'
             className='bg-[#f7ab0a] py-2 px-5 rounded-md text-black font-bold text-lg'>
               View website
             </button>
--- pages/index.tsx
+++ pages/index.tsx
@@ -52,7 +52,7 @@ const Home = ({ pageInfo, experiences, skills, projects, socials }: Props) => {
 
       {/* Projects */}
       <section id='projects' className='snap-start'>
-        <Projects/>
+        <Projects projects={ projects } />
       </section>
 
       {/* Contact Me */}
