e1bbd9ed43a66617aefc0d56fa5b8354afc8815a
fabio4520 <fabioleofc@gmail.com>
2022-12-26 09:51:37 -0500

    ADD: install next-sanity

--- components/Hero.tsx
+++ components/Hero.tsx
@@ -71,3 +71,22 @@ export default function Hero({ }: Props) {
     </div>
   )
 }
+
+export async function getStaticProps() {
+  const pageInfo:any = [
+    /* {
+      _createdAt: "2022-03-08T09:28:00Z",
+      _id: "1f69c53d-418a-452f-849a-e92466bb9c75",
+      _rev: "xnBg0xhUDzo561jnWODd5e",
+      _type: "pet",
+      _updatedAt: "2022-03-08T09:28:00Z",
+      name: "Capybara"
+    } */
+  ];
+
+  return {
+    props: {
+      pageInfo
+    }
+  };
+}
