05dd29ca0747e579cde4b0c9fcf60627c4066450
fabio4520 <fabioleofc@gmail.com>
2022-12-26 10:01:33 -0500

    ADD: configuration of sanity

--- components/Hero.tsx
+++ components/Hero.tsx
@@ -3,7 +3,7 @@ import { Cursor, useTypewriter } from 'react-simple-typewriter'
 import BackgroundCircles from './BackgroundCircles'
 import Link from 'next/link'
 import { motion } from 'framer-motion'
-
+import { client } from '../sanity'
 
 type Props = {}
 
@@ -73,16 +73,7 @@ export default function Hero({ }: Props) {
 }
 
 export async function getStaticProps() {
-  const pageInfo:any = [
-    /* {
-      _createdAt: "2022-03-08T09:28:00Z",
-      _id: "1f69c53d-418a-452f-849a-e92466bb9c75",
-      _rev: "xnBg0xhUDzo561jnWODd5e",
-      _type: "pet",
-      _updatedAt: "2022-03-08T09:28:00Z",
-      name: "Capybara"
-    } */
-  ];
+  const pageInfo:any = await client.fetch(`*[_type == "pet"]`);
 
   return {
     props: {
