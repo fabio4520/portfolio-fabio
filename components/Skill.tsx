2151439f55afb8b9a069d3e91df8efbc02b5add5
Fabio Fiestas <fabioleofc@gmail.com>
2024-04-02 01:37:33 -0500

    ADD: responsive for skills and hide proficiency

--- components/Skill.tsx
+++ components/Skill.tsx
@@ -13,14 +13,14 @@ export default function Skill({ directionLeft, imageLink, proficiency, alt }: Pr
     <div className='group relative flex cursor-pointer text-center'>
       <motion.img
         initial={{
-          x: directionLeft ? -200 : 200,
+          x: directionLeft ? -120 : 120,
           opacity: 0
         }}
         transition={{ duration: 1 }}
         whileInView={{ opacity: 1, x: 0 }}
         src={imageLink}
         alt={alt}
-        className='rounded-full border border-gray-500 object-contain w-20 h-20 xl:w-28 xl:h-28 filter group-hover:grayscale transition duration-300 ease-in-out'
+        className='rounded-full border border-gray-500 object-contain w-16 h-16 md:w-20 md:h-20 xl:w-28 xl:h-28 filter transition duration-300 ease-in-out'
         title={ alt }
       />
 
